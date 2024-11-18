892813c4f12fbb8ac0d74c98ca6a153a197f4806
Imran Irfan <imranbirfan@gmail.com>
2024-11-17 16:59:36 -0800

    refactored into a different file

--- ConvertDoc/convert_doc_form.py
+++ ConvertDoc/convert_doc_form.py
@@ -409,31 +409,3 @@ def remove_folder_recursive(folder):
             os.remove(os.path.join(root, name))
         for name in dirs:
             os.rmdir(os.path.join(root, name))
-
-
-def main():
-    root = Tk()
-    gui = convert_doc_gui(root)
-    minwidth = 854
-    minheight = 480
-
-    screenwidth = minwidth
-    screenheight = minheight
-
-    fullscreen_width = root.winfo_screenwidth()
-    fullscreen_height = root.winfo_screenheight()
-
-    if fullscreen_width == horizontal_resolution.UHD.value and fullscreen_height == vertical_resolution.UHD.value:
-        screenwidth = horizontal_resolution.QVGA.value
-        screenheight = vertical_resolution.QVGA.value
-
-    if fullscreen_width < horizontal_resolution.QHD.value and fullscreen_height < vertical_resolution.QHD.value:
-        screenwidth = horizontal_resolution.VGA.value
-        screenheight = vertical_resolution.VGA.value
-
-
-    root.geometry('%sx%s' % (screenwidth, screenheight))
-    root.mainloop()
-
-if __name__ == '__main__':
-    main()
