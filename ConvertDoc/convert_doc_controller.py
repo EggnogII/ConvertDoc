ed1fe9db4e41ac5b31730c894d551837a94bc5d1
Imran Irfan <imranbirfan@gmail.com>
2024-11-21 15:12:35 -0800

    Update formatting

--- ConvertDoc/convert_doc_controller.py
+++ ConvertDoc/convert_doc_controller.py
@@ -1,12 +1,8 @@
 from tkinter import filedialog
 
 def document_selector(selection_title, filetype):
-
-
     filename = filedialog.askopenfilename(title=selection_title, filetypes=
-
     (("Documents", f"*.{filetype}"), ("all files", "*.*")))
-
 
     return filename
 
